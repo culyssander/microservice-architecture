052b44f08680ef207389ced82bf3d33697e44fc2
Quitumba Ferreira <culyssandercentro@gmail.com>
2021-08-06 18:00:53 +0100

    microservice using springboot and spring cloud

---
+++ cloud-gateway/src/main/java/com/culysoft/cloudgateway/controller/FallbackController.java
@@ -0,0 +1,20 @@
+package com.culysoft.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+	@RequestMapping("/orderFallback")
+	public Mono<String> orderServiceFallback() {
+		return Mono.just("Order service is taking too long or is down. Please try again later");
+	}
+	
+	@RequestMapping("/paymentFallback")
+	public Mono<String> paymentServiceFallback() {
+		return Mono.just("Payment service is taking too long or is down. Please try again later");
+	}
+}
